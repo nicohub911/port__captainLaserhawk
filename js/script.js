22db80ae5d8d24c2d7991583c8b04c797c22533c
nicolas <nicolaser911@gmail.com>
2025-04-01 13:18:05 -0300

    new design

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+
+
+window.onload = function() {
+    document.getElementById('contenedor-carga').style.opacity = "0";
+    setTimeout(() => {
+        document.getElementById('contenedor-carga').style.display = 'none';    
+    }, 300);
+};
